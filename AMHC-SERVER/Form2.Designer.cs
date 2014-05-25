56506ec3c3dd7ba768032d8663881dc83892a116
XavierCHN <364671459@qq.com>
2014-05-25 22:50:12 +0800

    uplaod the sln

---
+++ AMHC-SERVER/Form2.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace AMHC_SERVER_CLIENT
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_start_server = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_start_server
+            // 
+            this.btn_start_server.Location = new System.Drawing.Point(420, 266);
+            this.btn_start_server.Name = "btn_start_server";
+            this.btn_start_server.Size = new System.Drawing.Size(100, 31);
+            this.btn_start_server.TabIndex = 0;
+            this.btn_start_server.Text = "手动启动服务器";
+            this.btn_start_server.UseVisualStyleBackColor = true;
+            this.btn_start_server.Click += new System.EventHandler(this.btn_start_server_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(532, 309);
+            this.Controls.Add(this.btn_start_server);
+            this.Name = "Form2";
+            this.Text = "AMHC-SERVER";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_start_server;
+    }
+}
---
+++ AMHC-SERVER/ServerForm.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+
+namespace AMHC_SERVER
+{
+    public partial class ServerForm : Form
+    {
+        public ServerForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                mythread = new Thread(new ThreadStart(BeginListen));
+                mythread.Start();
+            }
+            catch (System.Exception er)
+            {
+                MessageBox.Show(er.Message, "完成", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+            }
+        }
+        Thread mythread;
+        Socket socket;
+        // 清理所有正在使用的资源。
+        protected override void Dispose( bool disposing )
+        {
+            try
+　          {
+　　              socket.Close();//释放资源
+　　              mythread.Abort ( ) ;//中止线程
+　          }
+　          catch{ }
+            if( disposing )
+              {
+                   if (components != null)
+                   {
+                       components.Dispose();
+                   }
+              }
+              base.Dispose( disposing );
+         }
+         private void BeginListen()
+         {
+                string myAddr = "218.244.130.187";
+                IPAddress ServerIP = IPAddress.Parse(myAddr);
+                IPEndPoint iep = new IPEndPoint(ServerIP, 27000);
+                socket=new Socket(AddressFamily.InterNetwork,SocketType.Stream,ProtocolType.Tcp);
+                byte[] byteMessage=new byte[100]; 
+                this.label1.Text=iep.ToString();
+                socket.Bind(iep); 
+//              do
+                while(true)
+                {
+                   try
+                   {
+                       socket.Listen(5);
+                       Socket newSocket=socket.Accept();
+                       newSocket.Receive(byteMessage);
+                       string sTime = DateTime.Now.ToShortTimeString ( ) ;
+                       string msg=sTime+":"+"Message from:";
+                        msg+=newSocket.RemoteEndPoint.ToString()+Encoding.Default.GetString(byteMessage);
+                       this.listBox1.Items.Add(msg);
+                   }
+                   catch(SocketException ex)
+                   {
+                       this.label1.Text+=ex.ToString();
+                   }
+              }
+//            while(byteMessage!=null);
+         }
+    }
+}
